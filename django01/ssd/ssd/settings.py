08a26fe38e7cbda4272e05806801847637003c4d
Kate Wood <94301901+essexstudent1@users.noreply.github.com>
2022-07-08 11:04:43 -0400

    Update settings.py

--- django01/ssd/ssd/settings.py
+++ django01/ssd/ssd/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'report',                               # GROUP1 addition to enable reporting application
 ]
 
 MIDDLEWARE = [
