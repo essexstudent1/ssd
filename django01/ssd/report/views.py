58f16097416480197c69278c78906d17e3974b97
Kate Wood <94301901+essexstudent1@users.noreply.github.com>
2022-07-08 10:47:25 -0400

    Update views.py

--- django01/ssd/report/views.py
+++ django01/ssd/report/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Define the GROUP1 home page view
+def home_view(request):
+    user = request.user
+    return render (request, 'report/home.html')
+ 
