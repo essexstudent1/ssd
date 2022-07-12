acf0ce29ff5795e9504f846abdc1c87d24d7be9c
Kate Wood <94301901+essexstudent1@users.noreply.github.com>
2022-07-12 11:15:04 -0400

    Update forms.py

--- django01/ssd/report/forms.py
+++ django01/ssd/report/forms.py
@@ -5,7 +5,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
 
-from accounts.models import User
+from report.models import User
 
 # This section defines the form to register a new user
 
