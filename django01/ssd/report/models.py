0aad1b095b6b24ea71004ce2dd75512f9fec7317
Kate Wood <94301901+essexstudent1@users.noreply.github.com>
2022-07-12 11:26:38 -0400

    Update models.py

--- django01/ssd/report/models.py
+++ django01/ssd/report/models.py
@@ -8,7 +8,7 @@ from django.utils import timezone #GROUP1 addition
 
 class UserManager(BaseUserManager):
     
-    def create_user(self, email, password, security_question, security_answer):
+    def create_user(self, email, password, security_question, security_answer, firstName, lastName, address, town, province, country, postcode):
         if not email:
             raise ValueError(_('The Email must be set'))
         if not security_question:
@@ -17,7 +17,7 @@ class UserManager(BaseUserManager):
             raise ValueError(_('Security answer must be set'))
 
         email = self.normalize_email(email)
-        user = self.model(email=email, security_question=security_question, security_answer=security_answer)
+        user = self.model(email=email, security_question=security_question, security_answer=security_answer, firstName=firstName, lastName=lastName, address=address, town=town, province=province, country=country, postcode=postcode)
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -29,7 +29,7 @@ class User(AbstractBaseUser):
   firstName = models.CharField(verbose_name='First Name', help_text = 'Enter your first name.', max_length=100)
   lastName = models.CharField(verbose_name='Last Name', help_text = 'Enter your last name.', max_length=100)
   email = models.EmailField(verbose_name='Email Address', help_text = 'Enter your email address.',max_length=100, unique=True)
-  username = models.CharField(verbose_name='Username', help_text = 'Enter a username.',max_length=100, unique=True)
+  #username = models.CharField(verbose_name='Username', help_text = 'Enter a username.',max_length=100, unique=True)
   address = models.CharField(verbose_name='Street Address', help_text = 'Enter your street name and number, including unit number if applicable.',max_length=100)
   town = models.CharField(verbose_name='City or town', help_text = 'Enter your city, town, or village.',max_length=100)
   province = models.CharField(verbose_name='State or Province', help_text = 'Enter your state or province.',max_length=100)
@@ -44,8 +44,8 @@ class User(AbstractBaseUser):
   mfa_attempts = models.IntegerField(default=0)
    
   
-  USERNAME_FIELD = 'username'
-  REQUIRED_FIELDS = ['username', 'email']
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = ['security_question', 'security_answer']
 
   objects = UserManager()
 
